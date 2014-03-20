95fae2a65aa5b206a725a5551328156a303c6680
Jon Thornton <thornton.jon@gmail.com>
2014-03-20 14:55:24 -0400

    Converted htaccess routing to PHP code. Minimal testing.

--- html/index.php
+++ html/index.php
@@ -1,64 +1,261 @@
 <?php
 
-require_once('../includes/global.inc.php');
-require_once('class.FrontStream.php');
+// JT: Basic routing, replaces .htaccess. This is a dirty hack.
 
-showFrontPageForNotLoggedIn();
-
-$show_popup = get_input('p');
-if ($show_popup) {
-    // this is special url asked by Andrew to instant popup show. This parameter is not used in the code.
-    $_SESSION['front_page_open_invite_popup'] = true;
-    // redirecting to this page without this parameter in GET to show the popup only once
-    redirect($_SERVER['REQUEST_URI']);
+function redirect301($uri) {
+    header('HTTP/1.1 301 Moved Permanently');
+    header($uri);
 }
 
-if (get_input('redirect_to_get')) {
-   // all data posted is already processed in included FrontStream.php
-   redirect($_SERVER['REQUEST_URI']);
+// this is a simple utility function for debugging
+function pd($var) {
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+    die;
 }
 
-$show_invite_popup= 0;
-if (!empty($_SESSION['front_page_open_invite_popup'])) {
-    $show_invite_popup = 1;
-    unset($_SESSION['front_page_open_invite_popup']);
+// canonicalize on www.sharebloc.com
+if ($_SERVER['HTTP_HOST'] == 'sharebloc.com') {
+    redirect301('Location: http://www.sharebloc.com'.$_SERVER['REQUEST_URI']);
 }
 
-$content = FrontStream::getContent(FrontStream::POSTS_ON_PAGE, 0, array('type'=>'feed'));
-FrontStream::setCommonSmartyParams();
-
-$show_contest_widget = false;
-if (is_logged_in()) {
-    $user_follow_list = Utils::userData('following');
-    if(!empty($user_follow_list['tag_'.Utils::SALES_MARKETING_TAG_ID])) {
-        $show_contest_widget = true;
-    }
+if (preg_match('%^/contest([/].+)?$%', $_SERVER['REQUEST_URI'], $matches)) {
+    redirect301('/contest'.$matches[1]);
 }
 
-// todo remove after testing om beta
-if (!Utils::sVar('show_join_welcome_popup')) {
-    $_SESSION['show_join_welcome_popup'] = Utils::reqParam('test_welcome');
-}
+/* routing starts here */
 
-$smarty_params = array(
-    'content' => $content,
-    'show_invite_popup' => $show_invite_popup,
-    'active_submenu' => 'home',
-    'init_clipboard_copy' => true,
-    'show_join_welcome_popup' => Utils::unsetSVar('show_join_welcome_popup'),
-    'show_contest_widget' => $show_contest_widget,
-    'twitter_symbols_left' => Utils::countTwitterSymbolsLeft(" via @ShareBloc "),
-);
-
-$template->assign($smarty_params);
-$template->display('pages/index.tpl');
-
-function showFrontPageForNotLoggedIn() {
-    if (is_logged_in()) {
-        return;
-    }
-
-    $smarty = Utils::$smarty;
-    $smarty->display('pages/frontpage.tpl');
+if (preg_match('%^/$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('homepage.php');
     exit;
 }
+
+if (preg_match('%^/team/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('team.php');
+    exit;
+}
+
+if (preg_match('%^/terms/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('terms.php');
+    exit;
+}
+
+if (preg_match('%^/resources/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('resources.php');
+    exit;
+}
+
+if (preg_match('%^/every_sale_is_a_space_race/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('splash_space.php');
+    exit;
+}
+
+if (preg_match('%^/lead_farming_three_steps_to_grow_leads/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('lead_farming_three_steps_to_grow_leads.php');
+    exit;
+}
+
+if (preg_match('%^/back_office_human_resources/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('back_office_human_resources.php');
+    exit;
+}
+
+if (preg_match('%^/seven_questions_by_mobile_developers/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('seven_questions_by_mobile_developers.php');
+    exit;
+}
+
+if (preg_match('%^/privacy/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('privacy.php');
+    exit;
+}
+
+if (preg_match('%^/join/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('join.php');
+    exit;
+}
+
+if (preg_match('%^/signin/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('signin.php');
+    exit;
+}
+
+if (preg_match('%^/screenshots/(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    require('display_screenshot.php');
+    exit;
+}
+
+if (preg_match('%^/files/(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['file'] = $matches[1];
+    require('file_download.php');
+    exit;
+}
+
+if (preg_match('%^/companies/([^/]+)[/]?([^/]+)?[/]?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    $_GET['cmd'] = $matches[2];
+    require('vendor.php');
+    exit;
+}
+
+if (preg_match('%^/users/([^/]+)[/]?([^/]+)?[/]?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    $_GET['cmd'] = $matches[2];
+    require('users.php');
+    exit;
+}
+
+if (preg_match('%^/pw_rst/(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['reset_key'] = $matches[1];
+    require('new_password.php');
+    exit;
+}
+
+if (preg_match('%^/post/(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = $matches[1];
+    require('post.php');
+    exit;
+}
+
+if (preg_match('%^/invite/(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    require('process_invite.php');
+    exit;
+}
+
+if (preg_match('%^/share/(posted_link|question)s/([^/]+)?[/]?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = $matches[1];
+    $_GET['code_name'] = $matches[2];
+    $_GET['shared_post'] = 1;
+    require('show_post.php');
+    exit;
+}
+
+if (preg_match('%^/(posted_link|question|link)s/([^/]+)[/]?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = $matches[1];
+    $_GET['code_name'] = $matches[2];
+    require('show_post.php');
+    exit;
+}
+
+if (preg_match('%^/invite_experts(.+)?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['p'] = 1;
+    require('homepage.php');
+    exit;
+}
+
+if (preg_match('%^/join/(.+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = 'sbbeta_'.$matches[1];
+    require('process_invite.php');
+    exit;
+}
+
+if (preg_match('%^/blocs/$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('bloc.php');
+    exit;
+}
+
+if (preg_match('%^/blocs/([^/]+)[/]?([^/]+)?[/]?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    $_GET['cmd'] = $matches[2];
+    require('bloc.php');
+    exit;
+}
+
+if (preg_match('%^/unsubscribe/([^/]+)[/]([^/]+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = $matches[1];
+    $_GET['code'] = $matches[2];
+    require('unsubscribe.php');
+    exit;
+}
+
+if (preg_match('%^/recent/$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('recent_connections.php');
+    exit;
+}
+
+if (preg_match('%^/guidelines/$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('guidelines.php');
+    exit;
+}
+
+if (preg_match('%^/sitemap.xml$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('sitemap.php');
+    exit;
+}
+
+if (preg_match('%^/top_content_marketing_posts_of_2013$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['contest_id'] = 1;
+    require('contest.php');
+    exit;
+}
+
+if (preg_match('%^/top_content_marketing_posts_of_2013/(.+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['contest_id'] = 1;
+    $_GET['code'] = $matches[1];
+    require('contest_all.php');
+    exit;
+}
+
+if (preg_match('%^/content_marketing_nation$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['contest_id'] = 2;
+    require('contest.php');
+    exit;
+}
+
+if (preg_match('%^/content_marketing_nation/(.+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['contest_id'] = 2;
+    $_GET['code'] = $matches[1];
+    require('contest_all.php');
+    exit;
+}
+
+if (preg_match('%^/sharebloc_content_marketing_nation_contest_rules/?$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    require('contest_rules.php');
+    exit;
+}
+
+if (preg_match('%^/confirm/votes/([^/]+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = 'confirm_votes';
+    $_GET['code'] = $matches[1];
+    require('contest.php');
+    exit;
+}
+
+if (preg_match('%^/confirm/email/([^/]+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = 'confirm_email';
+    $_GET['code'] = $matches[1];
+    require('signin.php');
+    exit;
+}
+
+if (preg_match('%^/confirm/subscribe/([^/]+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = 'subscribe';
+    $_GET['code'] = $matches[1];
+    require('confirm.php');
+    exit;
+}
+
+if (preg_match('%^/img/([^/]+)(.png)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['code'] = $matches[1];
+    require('track_emails.php');
+    exit;
+}
+
+if (preg_match('%^/rss/(blocs|companies|users)/(.+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['type'] = $matches[1];
+    $_GET['code'] = $matches[2];
+    require('rss.php');
+    exit;
+}
+
+if (preg_match('%^/calendar/(.+)$%', $_SERVER['DOCUMENT_URI'], $matches)) {
+    $_GET['tag'] = $matches[1];
+    require('calendar.php');
+    exit;
+}
+
+// if we got here, it means that URL couldn't be routed. 404 time.
+require('404.php');
